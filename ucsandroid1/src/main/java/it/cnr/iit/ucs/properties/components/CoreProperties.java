e197991c3010446b1b7dbd646dfecf90d0aded2d
antoniolm23 <antoniolm23@gmail.com>
2019-07-18 16:11:37 +0200

    removed unnecessary project

--- ucsandroid1/src/main/java/it/cnr/iit/ucs/properties/components/CoreProperties.java
+++
@@ -1,6 +0,0 @@
-package it.cnr.iit.ucs.properties.components;
-
-import it.cnr.iit.ucs.properties.base.JournalProperties;
-import it.cnr.iit.ucs.properties.base.UriProperties;
-
-public interface CoreProperties extends UriProperties, JournalProperties {}
